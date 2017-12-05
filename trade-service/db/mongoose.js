5ee6f18d810c1328f5a0d53577c3710f2420590a
Neha Singh <nehasingh250691@gmail.com>
2017-12-05 18:32:11 +0530

    Updated trade-service-api

--- trade-service/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TradesData';
-
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI);
-
-module.exports = {mongoose};
