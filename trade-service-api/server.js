b6bc94d7a21dc4f31ac600078007b5e02823a241
Neha Singh <nehasingh250691@gmail.com>
2017-12-07 23:39:12 +0530

    TradeService announces itself to service registry

--- trade-service-api/server.js
+++ trade-service-api/server.js
@@ -3,7 +3,8 @@ const express = require('express'),
   app = express(),
   mongoose = require('mongoose'),
   Trade = require('./api/models/tradeListModel'),  
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  request = require('superagent');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
@@ -16,5 +17,18 @@ routes(app);
 
 app.listen(process.env.PORT, () =>{  
   console.log('trade list RESTful API server started on: ' + process.env.PORT); 
+  const announce = () => {
+    request.put(`${process.env.SERVICE_REGISTRY_ENDPOINT}/tradeService/${process.env.PORT}`, (err, response) =>{
+      if(err){
+        console.log(err);
+        console.log(`Error connecting to registry ${process.env.SERVICE_REGISTRY_ENDPOINT}`);
+        return;
+      }
+      console.log(response);
+    });
+  }
+
+  announce();
+  setInterval(announce, 15*1000);
 });
 
