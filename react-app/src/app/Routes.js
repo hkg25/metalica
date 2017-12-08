7c99328601ecc0b4b430bb85d6ecfb78f5dbc05a
hkg25 <harrygenius099@gmail.com>
2017-12-08 23:50:09 +0530

    Header added using material UI component

--- react-app/src/app/Routes.js
+++ react-app/src/app/Routes.js
@@ -5,6 +5,12 @@ import App from "./components/App";
 import Trade from "./components/trade/Trade";
 import Transfers from "./components/transfer/Transfer";
 import Transport from "./components/transport/Transport";
+ 
+// const App = () => (
+//   <MuiThemeProvider>
+//     <MyAwesomeReactComponent />
+//   </MuiThemeProvider>
+// );
 
 export default function Routes(props){
     return(
--- react-app/src/app/components/Header.js
+++ react-app/src/app/components/Header.js
@@ -1,20 +1,63 @@
 import React,{Component} from "react";
-
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Tabs, {Tab} from 'material-ui/Tabs';
 import {NavLink} from "react-router-dom";
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-export default class Header extends Component{
+const customstyles = theme => ({
+    root: {
+      flexGrow: 1,
+      marginTop: theme.spacing.unit * 3,
+    },
+  });
+  
+
+class Header extends Component{
     
-    constructor(props){
-        super(props);    
-    }
+    // constructor(props){
+    //     super(props);    
+    // }
+
+    state = {
+        value: 0,
+    };
+    
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
+    
 
     render(){
+        const { classes } = this.props;
         return(
             <div>
-                <NavLink to="/" exact className="button" activeClassName="success"> TRADES </NavLink>
-                <NavLink to="/transfers" className="button" activeClassName="success"> TRANSFERS </NavLink>
-                <NavLink to="/transports" className="button" activeClassName="success"> TRANSPORTS </NavLink>
+                <Paper className={classes.root}>
+                    <Tabs
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered >
+                    <Tab label="TRADES">
+                        <NavLink to="/" exact className="button" activeClassName="success"/>
+                    </Tab>
+                    <Tab label="TRANSFERS">
+                        <NavLink to="/transfers" className="button" activeClassName="success"/>
+                    </Tab>
+                    <Tab label="TRANSPORTS">
+                        <NavLink to="/transports" className="button" activeClassName="success"/>
+                    </Tab>
+        </Tabs>
+      </Paper>
             </div>
         )
     }
 }
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+export default withStyles(customstyles) (Header);
