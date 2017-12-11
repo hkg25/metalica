ccfccf6ef55ea2335970d6c21de1d5eb760f5e2a
hkg25 <harrygenius099@gmail.com>
2017-12-11 19:40:06 +0530

    Async await added

--- api-gateway/api_gateway.js
+++ api-gateway/api_gateway.js
@@ -7,18 +7,19 @@ console.log(discovery);
 
 const api_gateway = express();
 
-api_gateway.get('/:intent',(req,res,next) =>{
+api_gateway.get('/:intent', async (req,res,next) => {
     const serviceIntent = req.params.intent;
     console.log(`${req.method} /${serviceIntent} request received !`);
 
     const {serviceIp, servicePort} = await discovery(serviceIntent);
     console.log(`ip ${serviceIp} & port ${servicePort}`);
 
-    request.get(`http://${serviceIp}:${servicePort}/api/${serviceIntent}`).then((result) =>{
-        if(result)
-            res.json({err:{ message:`Failed to get response from ${serviceIntent} api !`}});
-        res.json({result});
-    });           
+    const result = await request.get(`http://${serviceIp}:${servicePort}/api/${serviceIntent}`);
+
+    if(result)
+    res.json({err:{ message:`Failed to get response from ${serviceIntent} api !`}});
+    res.json({result});
+ 
 });
 
 module.exports = api_gateway;
--- api-gateway/discovery.js
+++ api-gateway/discovery.js
@@ -1,6 +1,6 @@
 const request = require('superagent');
 
-module.exports.discovery = async function(serviceIntent){
+module.exports.discovery = function(serviceIntent){
     return new Promise(function(resolveFn, rejectFn){
         request.get(`${SERVICE_REGISTRY_ENDPOINT}/registry/${serviceIntent}`).then((result) => {
             if(result)
