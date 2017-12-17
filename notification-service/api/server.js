8e8ed515dd1753662880b738310b2516c4638949
Neha Singh <nehasingh250691@gmail.com>
2017-12-17 19:51:58 +0530

    eureka client added to register notification service

--- notification-service/api/server.js
+++ notification-service/api/server.js
@@ -1,23 +1,27 @@
-require("../config/config");
-const app = require('express')();
-const httpServer = require('http').Server(app);
-const io = require('socket.io')(httpServer);
-const Subscriber = require("./subscriber");
+require('../config/config')
 
-const PORT = process.env.PORT || 3000;
+const app = require('express')()
+const httpServer = require('http').Server(app)
+const io = require('socket.io')(httpServer)
 
+const Subscriber = require('./subscriber')
+const eurekaClient = require('../eureka-client')
 
-io.on('connection', function(socket){
-  socket.on('notification', function(msg){
-    var subscriber  = new Subscriber();
-    subscriber.receiveMessage().then((msg)=>{
-      io.emit('notification', msg);
-    }).catch((err)=>{
+var server = httpServer.listen(process.env.PORT, (err) => {
+  if (err) return console.log(`Unable to start the notification service api ${err}`)
+  console.log(`Notification service api started on ${server.address().address}:${server.address().port}`)
+  eurekaClient.start(function (error) {
+    console.log(error || 'complete')
+  })
 
-    });
-  });
-});
-
-httpServer.listen(PORT, function(){
-  console.log(`listening on ${PORT}`);
-});
+  io.on('connection', function (socket) {
+    socket.on('notification', function (msg) {
+      var subscriber = new Subscriber()
+      subscriber.receiveMessage().then((msg) => {
+        io.emit('notification', msg)
+      }).catch((err) => {
+        console.error(err)
+      })
+    })
+  })
+})
--- notification-service/api/subscriber.js
+++ notification-service/api/subscriber.js
@@ -1,61 +1,49 @@
-const amqp = require('amqplib/callback_api'),
-conn = require('./connect');
+const amqp = require('amqplib/callback_api')
+const conn = require('./connect')
 
 const QUEUE_NAME = process.env.QUEUE_NAME
 
 class Subscriber {
+  receiveMessage () {
+    conn.getChannel().then((ch) => {
+      ch.consume(QUEUE_NAME, function (msg) {
+        console.log(`${msg} get from the queue ${QUEUE_NAME}`)
+        return new Promise((resolve, reject) => {
+          resolve(msg)
+        })
+      }, {noAck: false})
+    }).catch((e) => {
+      reportErrors(e)
+    })
+  }
 
-receiveMessage() {
-    conn.getChannel().then( (ch) =>{
-        ch.consume(QUEUE_NAME, function(msg){
-          console.log(`${msg} get from the queue ${QUEUE_NAME}`);
-          return new Promise((resolve,reject)=>{
-            resolve(msg);
-          });
-        },{noAck:false});
-        
-    }).catch((e) =>{
-        reportErrors(e);
-    });        
+  reportErrors (err) {
+    console.log(err.stack)
+  }
 }
 
-reportErrors(err){  
-    console.log(err.stack);
-};
-}
+module.exports = Subscriber
 
-module.exports = Subscriber; 
+const {save} = require('../service/notification-service')
 
+const RABBITMQ_URL = process.env.BROKER_URL
 
+amqp.connect(RABBITMQ_URL, function (err, conn) {
+  // console.log("-------", conn)
+  if (err) return console.log(err)
+  conn.createChannel(function (err, ch) {
+    if (err) return console.log(err)
+    ch.assertQueue(QUEUE_NAME, {durable: false})
+    // ch.prefetch(1)
+    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', QUEUE_NAME)
+    ch.consume(QUEUE_NAME, function (msg) {
+      console.log(' [x] Received %s', msg.content.toString())
+      // Push notification to database
+      save(msg)
 
+      // Broadcast message to socket clients to receive notifications.
 
-
-
-
-
-
-
-
-const amqp = require('amqplib/callback_api');
-const {save} = require("../service/notification-service");
-
-const RABBITMQ_URL = process.env.BROKER_URL;
-const QUEUE_NAME= process.env.QUEUE_NAME;
-
-amqp.connect(RABBITMQ_URL, function(err, conn) {
-    //console.log("-------", conn);
-    conn.createChannel(function(err, ch) {
-       ch.assertQueue(QUEUE_NAME, {durable: false});
-       //ch.prefetch(1);
-       console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", QUEUE_NAME);
-       ch.consume(QUEUE_NAME, function(msg) {
-         console.log(" [x] Received %s", msg.content.toString());
-         //Push notification to database
-         save(msg);
-
-         //Broadcast message to socket clients to receive notifications.
-        
-       }, {noAck: false});
-       //conn.close();
-    });
-  });
+    }, {noAck: false})
+  // conn.close()
+  })
+})
