5c822a15105c9dd6d190b5b02a3d702cae2cb2ac
Neha Singh <nehasingh250691@gmail.com>
2017-12-23 19:30:21 +0530

    fixed api-gateway access issue

--- trade-service-api/eureka-client.js
+++ trade-service-api/eureka-client.js
@@ -2,7 +2,7 @@ const Eureka = require('eureka-js-client').Eureka
 
 const client = new Eureka({
   instance: {
-    app: 'trades',
+    app: 'trade-service',
     hostName: 'localhost',
     ipAddr: '127.0.0.1',
     statusPageUrl: `http://localhost:${process.env.PORT}`,
@@ -11,11 +11,12 @@ const client = new Eureka({
       '$': process.env.PORT,
       '@enabled': 'true'
     },
-    vipAddress: 'matellica.com/trade-service',
+    vipAddress: 'trade-service',
     dataCenterInfo: {
       '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
       name: 'MyOwn'
     }
+
   },
   eureka: {
     host: `${process.env.EUREKA_SERVER_HOST}`,
